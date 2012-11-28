dc98d19d782f2fb1dcd8236b5c5be2ae70b1935e
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-11-28 17:49:48 +0100

    Improved list builder in src/frontend/list-builder.lisp
    * src/frontend/list-builder.lisp (kind list): new method; extract and
      return kind
      (define-fundamental-accessor): define common accessors like `name',
      `qname', etc. via plist lookup
      (find-node eql 'list t): do not include if-does-not-exist keyword
      parameter in constructed node

--- src/frontend/list-builder.lisp
+++ src/frontend/list-builder.lisp
@@ -24,13 +24,48 @@
 
 (cl:in-package :rosetta.frontend)
 
-(defmethod qname ((thing list))
-  (getf (nthcdr 2 thing) :qname))
+;;; Format used by the `list' builder
+;;
+;; The output of the builder is list-based tree in which each node is
+;; a list of one of the two forms:
+;;
+;; 1. (KIND         CHILDREN PLIST)
+;; 2. ((:find KIND) nil      PLIST)
+;;
+;; Form 1. is produced by calls to `make-node' and form 2. is produced
+;; by calls to `find-node'. In both forms, PLIST is a plist of the
+;; keyword arguments passed to `make-node'/`find-node'. Calls to
+;; `add-child' modify nodes of form 1. by destructively adding child
+;; nodes to CHILDREN.
+
+(defmethod kind ((thing list))
+  (etypecase (first thing)
+    ((cons (eql :find))
+     (second (first thing)))
+    (keyword
+     (first thing))))
+
+(macrolet ((define-fundamental-accessor (name
+					 &optional
+					 (keyword (make-keyword name)) )
+	     `(defmethod ,name ((thing list))
+		(getf (nthcdr 2 thing) ,keyword))))
+
+  (define-fundamental-accessor name)
+  (define-fundamental-accessor qname)
+  (define-fundamental-accessor category)
+  (define-fundamental-accessor width)
+  (define-fundamental-accessor signed?)
+  (define-fundamental-accessor type1        :type)
+  (define-fundamental-accessor element-type)
+  (define-fundamental-accessor index-type))
 
 (defmethod find-node ((builder (eql 'list))
 		      (kind    t)
 		      &rest args &key &allow-other-keys)
-  (let+ (((kind nil &rest args) (apply #'make-node builder kind args)))
+  (let+ (((kind nil &rest args)
+	  (apply #'make-node builder kind
+		 (remove-from-plist args :if-does-not-exist))))
     (list* (list :find kind) nil args)))
 
 (defmethod make-node ((builder (eql 'list))
