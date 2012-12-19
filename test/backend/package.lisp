adef95d9842bbc5fe6b444996d942ffd96441136
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-12-19 03:07:43 +0100

    Extended ensure-serialization-cases in test/backend/package.lisp
    * test/backend/package.lisp (ensure-serialization-cases): added
      type-var parameter to allow clients using the type object

--- test/backend/package.lisp
+++ test/backend/package.lisp
@@ -157,6 +157,7 @@
 				       default-target
 				       (input-var           'input)
 				       (expected-var        'expected)
+				       (type-var            'type)
 				       (generated-var       'generated)
 				       destination-initform)
 				      cases
@@ -164,7 +165,6 @@
   "Execute BODY for each case in CASES which are of the form
 
   (TYPE-SPEC TARGET ((INPUT1 . EXPECTED1) ...))
-
 
 code is generated based on TYPE-SPEC and TARGET, compiled and
 GENERATED-VAR is bound to the function of one argument which is the
@@ -174,13 +174,13 @@ and compare the result of the function call with EXPECTED-VAR."
   `(ensure-cases (type-spec case-target inputs-and-expected)
        ,cases
 
-     (let+ ((type   (etypecase type-spec
-		      (symbol (make-instance type-spec))
-		      (list   (apply #'make-instance type-spec))
-		      (t      type-spec)))
-	    (target (or case-target ,default-target
-			(error "~@<Neither case-specific target nor ~S
-has been supplied.~@:>"
+     (let+ ((,type-var (etypecase type-spec
+			 (symbol (make-instance type-spec))
+			 (list   (apply #'make-instance type-spec))
+			 (t      type-spec)))
+	    (target    (or case-target ,default-target
+			   (error "~@<Neither case-specific target nor ~
+~S has been supplied.~@:>"
 			       :default-target)))
 	    ;; Generate code for TYPE and TARGET using fresh SOURCE,
 	    ;; DESTINATION and OFFSET variables.
