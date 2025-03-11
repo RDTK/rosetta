776745752927d91883a12a978428edfc3ef8bfe3
Jan Moringen <jan.moringen@posteo.de>
2025-03-11 20:53:30 +0100

    Remove some unnecessary stuff

--- cpack/CPackInclude.cmake
+++
@@ -1,8 +0,0 @@
-set(CPACK_CONFIG_FILE "" CACHE FILEPATH "Path to a CMake lists syntax file providing settings for CPack.")
-if(CPACK_CONFIG_FILE)
-    include(${CPACK_CONFIG_FILE})
-endif()
-
-message(STATUS "Using CPack package generator: ${CPACK_GENERATOR}")
-
-include(CPack)
